baff34b70698bf4f08bf66e251af0b7b016f7542
Josh Nussbaum <joshnuss@gmail.com>
2024-07-18 01:14:40 -0400

    Adds ast for delete statement

--- packages/vite-plugin/src/ast.ts
+++ packages/vite-plugin/src/ast.ts
@@ -1,4 +1,5 @@
 import { parse } from 'sql-parser-cst'
+import type { SelectStmt, DeleteStmt } from 'sql-parser-cst'
 
 interface Field {
   type: 'wildcard' | 'column',
@@ -11,15 +12,13 @@ interface Relation {
   alias: string | null
 }
 
-interface SelectStatement {
-  type: 'select',
+interface Statement {
+  type: 'select' | 'delete',
   fields: Field[],
   relations: Relation[]
 }
 
-type Statement = SelectStatement
-
-interface Options {
+export interface Options {
   dialect: 'sqlite' | 'bigquery' | 'mysql' | 'mariadb' | 'postgresql'
 }
 
@@ -34,70 +33,129 @@ export const ast = (sql: string, options: Options) : Statement[] => {
 
   return cst.statements.map((statement): Statement => {
     if (statement.type == 'select_stmt') {
-      const select_clause = statement.clauses.find((clause) => clause.type == 'select_clause')
-      const from_clause = statement.clauses.find((clause) => clause.type == 'from_clause')
-
-      const relations: Relation[] = []
-      const fields: Field[] = []
-
-      function addRelation(name: string, alias?: string) {
-        relations.push({
-          name,
-          alias: alias ?? null
-        })
-      }
-
-      if (from_clause?.type == 'from_clause') {
-        if (from_clause.expr.type == 'identifier') {
-          addRelation(from_clause.expr.name)
-        } else if (from_clause.expr.type == 'alias' && from_clause.expr.expr.type == 'identifier') {
-          addRelation(from_clause.expr.expr.name, from_clause.expr.alias.name)
-        } else if (from_clause.expr.type == 'join_expr') {
-
-          if (from_clause.expr.left.type == 'identifier')
-            addRelation(from_clause.expr.left.name)
-          else if (from_clause.expr.left.type == 'alias' && from_clause.expr.left.expr.type == 'identifier')
-            addRelation(from_clause.expr.left.expr.name, from_clause.expr.left.alias.name)
-
-          if (from_clause.expr.right.type == 'identifier')
-            addRelation(from_clause.expr.right.name)
-          else if (from_clause.expr.right.type == 'alias' && from_clause.expr.right.expr.type == 'identifier')
-            addRelation(from_clause.expr.right.expr.name, from_clause.expr.right.alias.name)
-        }
-      }
-
-      if (select_clause?.type == 'select_clause') {
-        select_clause.columns?.items.map((column) => {
-          if (column.type == 'all_columns') {
-            fields.push({
-              type: 'wildcard',
-              name: '*',
-              alias: null
-            })
-          } else if (column.type == 'identifier') {
-            fields.push({
-              type: 'column',
-              name: column.name,
-              alias: null
-            })
-          } else if (column.type == 'alias' && column.expr.type == 'identifier') {
-            fields.push({
-              type: 'column',
-              name: column.expr.name,
-              alias: column.alias.name
-            })
-          }
-        })
-      }
-
-      return {
-        type: 'select',
-        fields,
-        relations
-      }
+      return selectAst(statement)
+    } else if (statement.type == 'delete_stmt') {
+      return deleteAst(statement)
     }
 
     throw new UnsupportedCommandError(statement.type, sql)
   })
 }
 
+const selectAst = (statement: SelectStmt): Statement => {
+  const select_clause = statement.clauses.find((clause) => clause.type == 'select_clause')
+  const from_clause = statement.clauses.find((clause) => clause.type == 'from_clause')
+
+  const relations: Relation[] = []
+  const fields: Field[] = []
+
+  function addRelation(name: string, alias?: string) {
+    relations.push({
+      name,
+      alias: alias ?? null
+    })
+  }
+
+  if (from_clause?.type == 'from_clause') {
+    if (from_clause.expr.type == 'identifier') {
+      addRelation(from_clause.expr.name)
+    } else if (from_clause.expr.type == 'alias' && from_clause.expr.expr.type == 'identifier') {
+      addRelation(from_clause.expr.expr.name, from_clause.expr.alias.name)
+    } else if (from_clause.expr.type == 'join_expr') {
+
+      if (from_clause.expr.left.type == 'identifier')
+        addRelation(from_clause.expr.left.name)
+      else if (from_clause.expr.left.type == 'alias' && from_clause.expr.left.expr.type == 'identifier')
+        addRelation(from_clause.expr.left.expr.name, from_clause.expr.left.alias.name)
+
+      if (from_clause.expr.right.type == 'identifier')
+        addRelation(from_clause.expr.right.name)
+      else if (from_clause.expr.right.type == 'alias' && from_clause.expr.right.expr.type == 'identifier')
+        addRelation(from_clause.expr.right.expr.name, from_clause.expr.right.alias.name)
+    }
+  }
+
+  if (select_clause?.type == 'select_clause') {
+    select_clause.columns?.items.map((column) => {
+      if (column.type == 'all_columns') {
+        fields.push({
+          type: 'wildcard',
+          name: '*',
+          alias: null
+        })
+      } else if (column.type == 'identifier') {
+        fields.push({
+          type: 'column',
+          name: column.name,
+          alias: null
+        })
+      } else if (column.type == 'alias' && column.expr.type == 'identifier') {
+        fields.push({
+          type: 'column',
+          name: column.expr.name,
+          alias: column.alias.name
+        })
+      }
+    })
+  }
+
+  return {
+    type: 'select',
+    fields,
+    relations
+  }
+}
+
+const deleteAst = (statement: DeleteStmt): Statement => {
+  const fields: Field[] = []
+  const relations: Relation[] = []
+
+  const delete_clause = statement.clauses.find((clause) => clause.type == 'delete_clause')
+  const returning_clause = statement.clauses.find((clause) => clause.type == 'returning_clause')
+
+  if (delete_clause?.type == 'delete_clause') {
+    delete_clause.tables.items.forEach((table) => {
+      if (table.type == 'identifier') {
+        relations.push({
+          name: table.name,
+          alias: null
+        })
+      } else if (table.type == 'alias' && table.expr.type == 'identifier') {
+        relations.push({
+          name: table.expr.name,
+          alias: table.alias.name
+        })
+      }
+    })
+  }
+
+  if (returning_clause?.type == 'returning_clause' && returning_clause.columns.type == 'list_expr') {
+    returning_clause.columns.items.forEach((column) => {
+      if (column.type == 'all_columns') {
+        fields.push({
+          type: 'wildcard',
+          name: '*',
+          alias: null
+        })
+      } else if (column.type == 'identifier') {
+        fields.push({
+          type: 'column',
+          name: column.name,
+          alias: null
+        })
+      } else if (column.type == 'alias' && column.expr.type == 'identifier') {
+        fields.push({
+          type: 'column',
+          name: column.expr.name,
+          alias: column.alias.name
+        })
+      }
+    })
+  }
+
+  return {
+    type: 'delete',
+    fields,
+    relations
+  }
+}
--- packages/vite-plugin/test/ast.test.ts
+++ packages/vite-plugin/test/ast.test.ts
@@ -1,103 +1,198 @@
 import { ast } from '../src/ast.js'
+import type { Options } from '../src/ast.ts'
 
-const options = {
+const options: Options = {
   dialect: 'postgresql'
 }
 
-describe('ast', () => {
-  test('unsuported statement', () => {
-    expect(() => {
-      ast('truncate table products', options)
-    }).toThrowError('Unsupported statement `truncate_stmt` `truncate table products`')
+test('unsuported statement', () => {
+  expect(() => {
+    ast('truncate table products', options)
+  }).toThrowError('Unsupported statement `truncate_stmt` `truncate table products`')
+})
+
+describe('select', () => {
+  test('star query', () => {
+    const result = ast('select * from products', options)
+
+    expect(result).toMatchObject([{
+      type: 'select',
+      fields: [
+        { type: 'wildcard', name: '*' }
+      ],
+      relations: [
+        { name: 'products', alias: null }
+      ]
+    }])
   })
-  describe('select', () => {
-    test('star query', () => {
-      const result = ast('select * from products', options)
 
-      expect(result).toMatchObject([{
-        type: 'select',
-        fields: [
-          { type: 'wildcard', name: '*' }
-        ],
-        relations: [
-          { name: 'products', alias: null }
-        ]
-      }])
-    })
+  test('multiple from', () => {
+    const result = ast('select * from products, categories', options)
 
-    test('multiple from', () => {
-      const result = ast('select * from products, categories', options)
+    expect(result).toMatchObject([{
+      type: 'select',
+      fields: [
+        { type: 'wildcard', name: '*' }
+      ],
+      relations: [
+        { name: 'products', alias: null },
+        { name: 'categories', alias: null }
+      ]
+    }])
+  })
 
-      expect(result).toMatchObject([{
-        type: 'select',
-        fields: [
-          { type: 'wildcard', name: '*' }
-        ],
-        relations: [
-          { name: 'products', alias: null },
-          { name: 'categories', alias: null }
-        ]
-      }])
-    })
+  test('with from alias', () => {
+    const result = ast('select * from products as prod', options)
 
-    test('with from alias', () => {
-      const result = ast('select * from products as prod', options)
+    expect(result).toMatchObject([{
+      type: 'select',
+      fields: [
+        { type: 'wildcard', name: '*' }
+      ],
+      relations: [
+        { name: 'products', alias: 'prod' }
+      ]
+    }])
+  })
 
-      expect(result).toMatchObject([{
-        type: 'select',
-        fields: [
-          { type: 'wildcard', name: '*' }
-        ],
-        relations: [
-          { name: 'products', alias: 'prod' }
-        ]
-      }])
-    })
+  test('with multiple from alias', () => {
+    const result = ast('select * from products as prod, categories as cat', options)
 
-    test('with multiple from alias', () => {
-      const result = ast('select * from products as prod, categories as cat', options)
+    expect(result).toMatchObject([{
+      type: 'select',
+      fields: [
+        { type: 'wildcard', name: '*' }
+      ],
+      relations: [
+        { name: 'products', alias: 'prod' },
+        { name: 'categories', alias: 'cat' }
+      ]
+    }])
+  })
 
-      expect(result).toMatchObject([{
-        type: 'select',
-        fields: [
-          { type: 'wildcard', name: '*' }
-        ],
-        relations: [
-          { name: 'products', alias: 'prod' },
-          { name: 'categories', alias: 'cat' }
-        ]
-      }])
-    })
+  test('with column names', () => {
+    const result = ast('select id, name, price from products', options)
 
-    test('with column names', () => {
-      const result = ast('select id, name, price from products', options)
+    expect(result).toMatchObject([{
+      type: 'select',
+      fields: [
+        { type: 'column', name: 'id' },
+        { type: 'column', name: 'name' },
+        { type: 'column', name: 'price' }
+      ],
+      relations: [
+        { name: 'products', alias: null },
+      ]
+    }])
+  })
 
-      expect(result).toMatchObject([{
-        type: 'select',
-        fields: [
-          { type: 'column', name: 'id' },
-          { type: 'column', name: 'name' },
-          { type: 'column', name: 'price' }
-        ],
-        relations: [
-          { name: 'products', alias: null },
-        ]
-      }])
-    })
+  test('with column aliases', () => {
+    const result = ast('select id, price as amount from products', options)
 
-    test('with column aliases', () => {
-      const result = ast('select id, price as amount from products', options)
+    expect(result).toMatchObject([{
+      type: 'select',
+      fields: [
+        { type: 'column', name: 'id' },
+        { type: 'column', name: 'price', alias: 'amount' }
+      ],
+      relations: [
+        { name: 'products', alias: null },
+      ]
+    }])
+  })
+})
 
-      expect(result).toMatchObject([{
-        type: 'select',
-        fields: [
-          { type: 'column', name: 'id' },
-          { type: 'column', name: 'price', alias: 'amount' }
-        ],
-        relations: [
-          { name: 'products', alias: null },
-        ]
-      }])
-    })
+describe('delete', () => {
+  test('returns nothing', () => {
+    const result = ast('delete from products', options)
+
+    expect(result).toMatchObject([{
+      type: 'delete',
+      fields: [],
+      relations: [
+        { name: 'products', alias: null },
+      ]
+    }])
+  })
+
+  test('returns star', () => {
+    const result = ast('delete from products returning *', options)
+
+    expect(result).toMatchObject([{
+      type: 'delete',
+      fields: [
+        {
+          type: 'wildcard',
+          name: '*'
+        }
+      ],
+      relations: [
+        { name: 'products', alias: null },
+      ]
+    }])
+  })
+
+  test('returns field names', () => {
+    const result = ast('delete from products returning id, name', options)
+
+    expect(result).toMatchObject([{
+      type: 'delete',
+      fields: [
+        {
+          type: 'column',
+          name: 'id'
+        },
+        {
+          type: 'column',
+          name: 'name'
+        }
+      ],
+      relations: [
+        { name: 'products', alias: null },
+      ]
+    }])
+  })
+
+  test('returns field alias', () => {
+    const result = ast('delete from products returning id, price as amount', options)
+
+    expect(result).toMatchObject([{
+      type: 'delete',
+      fields: [
+        {
+          type: 'column',
+          name: 'id'
+        },
+        {
+          type: 'column',
+          name: 'price',
+          alias: 'amount'
+        }
+      ],
+      relations: [
+        { name: 'products', alias: null },
+      ]
+    }])
+  })
+
+  test('with from alias', () => {
+    const result = ast('delete from products as prod returning id, name', options)
+
+    expect(result).toMatchObject([{
+      type: 'delete',
+      fields: [
+        {
+          type: 'column',
+          name: 'id'
+        },
+        {
+          type: 'column',
+          name: 'name'
+        }
+      ],
+      relations: [
+        { name: 'products', alias: 'prod' },
+      ]
+    }])
   })
 })
